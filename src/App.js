f45010c9437bfa2c51376e742bedaae7503b98d0
unknown <akreminourhene@gmail.com>
2021-12-09 09:17:29 +0100

    firstreactcheckpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import{Container,Row,Col,Form} from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+     <div className='example'>
+      <Container>
+        <Row>
+            <Col xs={{order: 'first' }}>why</Col>
+            <Col xs>react</Col>
+            <Col xs={{order: 'last' }}>is hard</Col>
+        </Row> 
+      </Container>
+      <div className='form'>
+              <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Email
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control plaintext readOnly defaultValue="email@example.com" />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+        </Form>
+        <div/>
+      <div/>
+    </div>
     </div>
   );
 }
